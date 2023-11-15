ec602e7a3d3e6dba35d32fcdf774d45984f2df1b
T <itthi91@gmail.com>
2023-11-15 22:19:40 +0700

    feat(assignment): submit Boolean and Logical Operations

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,13 @@
+// Exercise #1: Event Conditions
+// Start coding here
 let isOver18;
 let hasCriminalBlacklist;
-
 let isAllow;
 
-// Start coding here
+// james's conditions 
+isOver18 = true; // he's 18
+hasCriminalBlacklist = false; // no criminal records
 
+// result 
+isAllow = isOver18 && !hasCriminalBlacklist;
 console.log(isAllow);
